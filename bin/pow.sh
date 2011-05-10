f4d4b3a55de5f655c88832211aa2e599f34688a2
Julien Portalier <ysbaddaden@gmail.com>
2011-05-10 09:11:38 +0200

    Replaced ruby startup script with a shell one, since there may not be any system ruby available (and RVM isn't loaded on session start).

---
+++ bin/pow.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+POW_ROOT=`dirname $(pwd -P)`
+POW_BIN="$POW_ROOT/bin/`basename $0`"
+NODE_PATH="$POW_ROOT/node_modules:$NODE_PATH"
+
+export NODE_PATH POW_BIN
+exec "$POW_ROOT/bin/node" "$POW_ROOT/lib/command.js"
+
