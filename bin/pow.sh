d41ef5d521038afc3ff4fc9ff2b1571b407f94d5
Julien Portalier <ysbaddaden@gmail.com>
2012-02-09 00:02:50 +0100

    Revert "We no longer need bin/pow.sh let's use bin/pow directly."
    This reverts commit 7226287ca17022edef6ddbedf507d168e025d9f9.

---
+++ bin/pow.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+# detects the correct path of current script
+cd `dirname $0`
+POW_ROOT=`dirname $(pwd -P)`
+
+POW_BIN="$POW_ROOT/bin/`basename $0`"
+NODE_PATH="$POW_ROOT/node_modules:$NODE_PATH"
+
+export NODE_PATH POW_BIN
+
+# loads user config if available
+if [ -f ~/.powconfig ] ; then
+  source ~/.powconfig
+fi
+
+# loads RVM if available
+if [ -f ~/.rvm/scripts/rvm ] ; then
+  source ~/.rvm/scripts/rvm
+elif [ -f /usr/local/rvm/scripts/rvm ] ; then
+  source /usr/local/rvm/scripts/rvm
+fi
+
+# kills any running instance
+PID=`ps x | awk -F " " "{ if ( \\$5 == \\"$POW_ROOT/bin/node\\" && \\$6 == \\"$POW_ROOT/lib/command.js\\" ) print \\$1 }"`
+[ "$PID" == "" ] || kill $PID
+
+# starts instance
+exec "$POW_ROOT/bin/node" "$POW_ROOT/lib/command.js" $*
+
